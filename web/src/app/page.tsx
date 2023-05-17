26a427915d694573a98ba05d43c00d385986d647
Lucas Tannús Vieira <lucastannus@Lucas-Tannus-MacBook-Pro-ArcTouch-542A.local>
2023-05-17 18:40:34 -0300

    feat: add default fonts

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <h1 className="h-screen p-20 font-medium">Hello world</h1>
+  return (
+    <div className="">
+      {/* Left */}
+      <div></div>
+
+      {/* Right */}
+      <div></div>
+    </div>
+  )
 }
