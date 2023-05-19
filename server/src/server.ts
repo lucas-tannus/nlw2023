b166bb79201f890590083c1a75c071e316a6aaf1
Lucas Tannús <lucastannusvieira@gmail.com>
2023-05-19 09:56:25 -0300

    feat: add token creation endpoint

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,16 @@
+import 'dotenv/config'
 import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
 
 import cors from '@fastify/cors'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
 app.register(cors, {
   origin: true,
 })
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
