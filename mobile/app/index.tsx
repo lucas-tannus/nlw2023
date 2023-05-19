c6bce8990a9b80ed13c04666faf6849c720da6a0
Lucas Tannús <lucastannusvieira@gmail.com>
2023-05-19 15:21:19 -0300

    feat: add authentication to app application

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,10 +1,9 @@
 import { StatusBar } from 'expo-status-bar'
 import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
-
-import blurBg from './src/assets/luz.png'
-
-import Stripes from './src/assets/stripes.svg'
-import NLWLogo from './src/assets/logo.svg'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import React, { useEffect } from 'react'
+import * as SecureStore from 'expo-secure-store'
+import { useRouter } from 'expo-router'
 
 import {
   useFonts,
@@ -12,17 +11,59 @@ import {
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from '../src/assets/luz.png'
+import Stripes from '../src/assets/stripes.svg'
+import NLWLogo from '../src/assets/logo.svg'
+
 import { styled } from 'nativewind'
-import React from 'react'
+import { api } from '../src/lib/api'
 
 const SytledStripes = styled(Stripes)
 
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/47e1ad4341997f8704cf',
+}
+
 export default function App() {
+  const router = useRouter()
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  const [, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: '47e1ad4341997f8704cf',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery,
+  )
+
+  async function handleGithubOAuthCode(code: string) {
+    try {
+      const tokenResponse = await api.post('/register', { code })
+      const { token } = tokenResponse.data
+      await SecureStore.setItemAsync('token', token)
+      router.push('/memories')
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params
+      handleGithubOAuthCode(code)
+    }
+  }, [response])
 
   if (!hasLoadedFonts) {
     return null
@@ -51,6 +92,7 @@ export default function App() {
         <TouchableOpacity
           className="rounded-full bg-green-500 px-5 py-2"
           activeOpacity={0.7}
+          onPress={() => signInWithGithub()}
         >
           <Text className="font-alt text-sm uppercase text-black">
             Começar a cadastrar
