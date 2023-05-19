97e6047bf8482e7e3caaf06d4a3b6842e4613ceb
Lucas Tannús <lucastannusvieira@gmail.com>
2023-05-19 16:53:11 -0300

    feat: creating new memory page and changing layout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,9 +1,15 @@
 import { ReactNode } from 'react'
+import { cookies } from 'next/headers'
 
 import {
   Roboto_Flex as Roboto,
   Bai_Jamjuree as BaiJamjuree,
 } from 'next/font/google'
+
+import Hero from '@/components/Hero'
+import SignIn from '@/components/SignIn'
+import UserProfile from '@/components/UserProfile'
+import Copyright from '@/components/Copyright'
 
 import './globals.css'
 
@@ -20,12 +26,25 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const isAuthenticated = cookies().get('token')
+
   return (
     <html lang="en">
       <body
         className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
       >
-        {children}
+        <main className="grid min-h-screen grid-cols-2">
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/stars.svg)] bg-cover px-20 py-16">
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+            {isAuthenticated ? <UserProfile /> : <SignIn />}
+            <Hero />
+            <Copyright />
+          </div>
+          <div className="flex flex-col bg-[url(../assets/stars.svg)] bg-cover p-16">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   )
