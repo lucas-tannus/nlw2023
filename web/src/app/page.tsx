025b74402e5b7ecbb1a9d6c35da2fe7a5971f924
Lucas Tannús <lucastannusvieira@gmail.com>
2023-05-19 11:19:00 -0300

    feat: add web login flow

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,15 +1,19 @@
+import { cookies } from 'next/headers'
 import SignIn from '@/components/SignIn'
 import Copyright from '../components/Copyright'
 import Hero from '../components/Hero'
 import EmptyMessage from '@/components/EmptyMessage'
+import UserProfile from '@/components/UserProfile'
 
 export default function Home() {
+  const isAuthenticated = cookies().get('token')
+
   return (
     <main className="grid min-h-screen grid-cols-2">
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/stars.svg)] bg-cover px-20 py-16">
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
-        <SignIn />
+        {isAuthenticated ? <UserProfile /> : <SignIn />}
         <Hero />
         <Copyright />
       </div>
