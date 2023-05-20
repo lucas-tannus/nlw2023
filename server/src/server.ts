ff8da676593707417ceeb769d006607e2ce7d43c
Lucas Tannús <lucastannusvieira@gmail.com>
2023-05-20 18:21:01 -0300

    feat: create upload endpoint at the server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,23 @@
 import 'dotenv/config'
 import fastify from 'fastify'
-import { memoriesRoutes } from './routes/memories'
 
 import cors from '@fastify/cors'
+import multipart from '@fastify/multipart'
 import jwt from '@fastify/jwt'
+import fastifyStatic from '@fastify/static'
+
+import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
 
+app.register(fastifyStatic, {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+app.register(multipart)
 app.register(cors, {
   origin: true,
 })
@@ -16,6 +26,7 @@ app.register(jwt, {
 })
 app.register(authRoutes)
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
